5d51f2a8a714ffd064afe16bbe6ed13970a406e0
Thkksn <thakronkhamsingnok@gmail.com>
2026-03-04 16:31:25 +0700

    new: new mvc

---
+++ BackOfficeManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BackOfficeManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
